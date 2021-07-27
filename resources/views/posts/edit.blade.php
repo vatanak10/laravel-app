d51dc0cf2a5ef896d20f6993546e8e93cf83965f
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2021-07-27 11:47:46 +0700

    :sparkles:add new edit and delete post features

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Edit Posts</h1>
+    {!! Form::open(['action' => ['App\Http\Controllers\PostsController@update', $post->id], 'method' => 'POST']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', $post->title, ['class' => 'form-control', 'placeholder' => 'Title'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('body', 'Body')}}
+            {{Form::textarea('body', $post->body, ['id' => 'editor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
+        </div>
+        {{Form::hidden('_method', 'PUT')}}
+        {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+    {!! Form::close() !!}
+@endsection
+
+@section('ckeditor')
+    <script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
+
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts" class="btn btn-default">Back</a>
+    <a href="/posts" class="btn btn-outline-dark">Back</a>
     <h1>{{$post->title}}</h1>
     <p>{!!$post->body!!}</p>
     <hr>
     <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-info">Edit</a>
+
+    {!!Form::open(['action' => ['App\Http\Controllers\PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form:: submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
